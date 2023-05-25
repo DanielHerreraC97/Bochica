5d1a721562ec28ee39dd04763b52df29bbc77beb
JulianGalvis.zip <105403435+Juliandron@users.noreply.github.com>
2023-05-25 16:17:46 -0500

    Prefab contador

--- Bochica/Assets/Scripts/Item.cs
+++ Bochica/Assets/Scripts/Item.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class Item : MonoBehaviour
 {
     int items = 1;
+    public AudioSource pickUpSound;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            pickUpSound.Play();
             Debug.Log("Pglo");
             ScoreManager.AddScore(items);
             Destroy(this.gameObject);
