43e6262dae68bfb062533e41df2a3d17dce36779
JulianGalvis.zip <105403435+Juliandron@users.noreply.github.com>
2023-05-31 23:29:41 -0500

    Update FadeOutScene2.cs

--- Bochica/Assets/Scripts/FadeOutScene2.cs
+++ Bochica/Assets/Scripts/FadeOutScene2.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class FadeOutScene2 : MonoBehaviour
 {
     public SpriteRenderer spriteFadeOut;
+    public TMP_Text text;
     void Start()
     {
+        text.DOFade(1, 2);
         spriteFadeOut.DOFade(0, 2);
     }
 
